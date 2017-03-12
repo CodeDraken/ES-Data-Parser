727d8dfe257da9c4068a467b92bb91af4c2ed639
CodeDraken <codedraken@gmail.com>
2017-03-12 11:43:58 -0700

    Add more attributes

--- ship-scraper.js
+++ ship-scraper.js
@@ -50,87 +50,74 @@ const layoutSelector = (ship, retry) => {
     const engines = ship.match(/engine (-?\d.*)/g);
     const guns = ship.match(/gun (-?\d.*)/g);
     const turrets = ship.match(/turret (-?\d.*)/g);
+    const fighter = ship.match(/fighter (-?\d.*)/g);
+
     const explosions = ship.match(/explode (".*)/g);
 
     return {
       engines: Array.isArray(engines) ? engines.map( item => item.replace(/[a-zA-Z]*/g, '').trim()) : engines,
+      explosions,
+      fighter,
       guns,
       turrets,
-      explosions
     };
   } catch (err) {
     //console.log('layout error: ', ship, err)
     if(!retry) {
-      print('a ship failed retrying..');
+      console.warn('a ship failed retrying..');
       layoutSelector(ship[0], true);
+    } else {
+      console.error('Retry failed');
     }
   }
+};
+
+const scrapeShip = (data) => {
+  return {
+    name: attrSelector('ship', data, true),
+    sprite: attrSelector('sprite', data, true),
+
+    attributes: {
+      automation: attrSelector('automation', data),
+      bunks: attrSelector('bunks', data),
+      cargoSpace: attrSelector('cargo space', data),
+      category: attrSelector('category', data, true),
+      cost: attrSelector('cost', data),
+      drag: attrSelector('drag', data),
+      engineCap: attrSelector('engine capacity', data),
+      fuelCap: attrSelector('fuel capacity', data),
+      heat: attrSelector('heat dissipation', data),
+      hull: attrSelector('hull', data),
+      mass: attrSelector('mass', data),
+      outfitSpace: attrSelector('outfit space', data),
+      requiredCrew: attrSelector('required crew', data),
+      shields: attrSelector('shields', data),
+      weaponCap: attrSelector('weapon capacity', data),
+
+      weapon: {
+        blastRadius: attrSelector('blast radius', data),
+        hitForce: attrSelector('hit force', data),
+        hullDamage: attrSelector('hull damage', data),
+        shieldDamage: attrSelector('shield damage', data),
+      }
+    },
+
+    outfits: outfitSelector(data),
+
+    layout: layoutSelector(data),
+    
+    description: attrSelector('description', data)
+  };
 };
 
 const shipGenerator = (faction, data) => {
   if( data && data.length > 1 && Array.isArray(data) ) {
     for (var i=0; i<data.length; i++) {
-      const ship = {
-        name: attrSelector('ship', data[i], true),
-        sprite: attrSelector('sprite', data[i], true),
-
-        attributes: {
-          category: attrSelector('category', data[i], true),
-          cost: attrSelector('cost', data[i]),
-          hull: attrSelector('hull', data[i]),
-          mass: attrSelector('mass', data[i]),
-          drag: attrSelector('drag', data[i]),
-          heat: attrSelector('heat dissipation', data[i]),
-          outfitSpace: attrSelector('outfit space', data[i]),
-          weaponCap: attrSelector('weapon capacity', data[i]),
-          engineCap: attrSelector('engine capacity', data[i]),
-          automation: attrSelector('automation', data[i]),
-          weapon: {
-            blastRadius: attrSelector('blast radius', data[i]),
-            shieldDamage: attrSelector('shield damage', data[i]),
-            hullDamage: attrSelector('hull damage', data[i]),
-            hitForce: attrSelector('hit force', data[i])
-          }
-        },
-
-        outfits: outfitSelector(data[i]),
-
-        layout: layoutSelector(data[i]),
-        
-        description: attrSelector('description', data[i])
-      };
+      const ship = scrapeShip(data[i]);
       ships[faction][ship.name.toLowerCase()] = ship;
     }
   } else if(data) {
-    const ship = {
-      name: attrSelector('ship', data, true),
-      sprite: attrSelector('sprite', data, true),
-
-      attributes: {
-        category: attrSelector('category', data, true),
-        cost: attrSelector('cost', data),
-        hull: attrSelector('hull', data),
-        mass: attrSelector('mass', data),
-        drag: attrSelector('drag', data),
-        heat: attrSelector('heat dissipation', data),
-        outfitSpace: attrSelector('outfit space', data),
-        weaponCap: attrSelector('weapon capacity', data),
-        engineCap: attrSelector('engine capacity', data),
-        automation: attrSelector('automation', data),
-        weapon: {
-          blastRadius: attrSelector('blast radius', data),
-          shieldDamage: attrSelector('shield damage', data),
-          hullDamage: attrSelector('hull damage', data),
-          hitForce: attrSelector('hit force', data)
-        }
-      },
-
-      outfits: outfitSelector(data),
-
-      layout: layoutSelector(data),
-      
-      description: attrSelector('description', data)
-    };
+    const ship = scrapeShip(data);
     
     ships[faction][ship.name.toLowerCase()] = ship;
   }
